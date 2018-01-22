79c6abd1f840bf79b4dc9f3586f540f70fae033d
Peter Burns <peterburnshicks@gmail.com>
2018-01-22 00:40:43 -0500

    initial files from codepen

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/Peter-Burns/pen/mqGpGQ.
+
+ Made a portfolio page to work on my bootstrap skills. Pretty happy about the scrolling navbar changes.
---
+++ js/index.js
@@ -0,0 +1,19 @@
+var aboutBottom = $('#aboutMe').offset().top + $('#aboutMe').height();
+var portfolioBottom = aboutBottom + $('#portfolio').height();
+$(window).on('scroll',function(){
+   $('li').attr('class','');
+    var stop = Math.round($(window).scrollTop());
+    if (stop > portfolioBottom) {
+        $('#cont').attr('class','active');
+    } 
+    else if(stop > aboutBottom) {
+       $('#port').attr('class','active');
+    }
+    else{
+       $('#top').attr('class','active'); 
+    }
+});
+$('.btn').on('click',function(e){
+  e.preventDefault();
+  alert('form not yet implemented');
+});
